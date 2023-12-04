0e6e0afa7bec54e26c52a97aebf4645e791e5e95
Michael Kaleja <michael.kaleja@m-net.de>
2023-12-04 11:03:30 +0100

    day4 a1 completed

--- modules/day4/src/main/scala/de/mk/aoc/a1/Main.scala
+++ modules/day4/src/main/scala/de/mk/aoc/a1/Main.scala
@@ -1,7 +1,48 @@
 package de.mk.aoc.a1
 
 import cats.effect.{ExitCode, IO, IOApp}
+import cats.syntax.all._
+import de.mk.util.Util
+
+import scala.util.Try
 
 object Main extends IOApp {
-  override def run(args: List[String]): IO[ExitCode] = ???
+
+  def matchesPerLine(s: String): Int = {
+    val relevant = s.substring(s.indexOf(":"))
+    relevant.split("""\|""").toList match {
+      case ::(head, ::(next, _)) =>
+        val r = "\\d+".r
+
+        val winningNums = r
+          .findAllMatchIn(head)
+          .toList
+          .mapFilter(m => Try(m.group(0).toInt).toOption)
+
+        val tips = r
+          .findAllMatchIn(next)
+          .toList
+          .mapFilter(m => Try(m.group(0).toInt).toOption)
+
+        val matches = winningNums.intersect(tips)
+
+        val points =
+          if (matches.isEmpty) 0
+          else
+            matches.zipWithIndex.map { case (_, index) =>
+              if (index == 0) 1 else 2
+            }.product
+        points
+      case _ => 0
+    }
+  }
+
+  override def run(args: List[String]): IO[ExitCode] =
+    Util.getInput
+      .use { bs =>
+        val lines = bs.getLines().toList
+        val matches = lines.map(matchesPerLine)
+        IO.print(matches.sum)
+      }
+      .as(ExitCode.Success)
 }
