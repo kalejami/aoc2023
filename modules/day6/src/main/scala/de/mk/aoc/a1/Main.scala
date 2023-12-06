89ac4ccab0e014fac574418c63bc1c1495ba06fa
Michael Kaleja <michael.kaleja@m-net.de>
2023-12-06 08:58:07 +0100

    day6 a1 + a2 completed

--- modules/day6/src/main/scala/de/mk/aoc/a1/Main.scala
+++ modules/day6/src/main/scala/de/mk/aoc/a1/Main.scala
@@ -1,7 +1,37 @@
 package de.mk.aoc.a1
 
 import cats.effect.{ExitCode, IO, IOApp}
+import de.mk.util.Util
+
+import scala.annotation.tailrec
 
 object Main extends IOApp {
-  override def run(args: List[String]): IO[ExitCode] = ???
+  final case class Race(time: Long, distance: Long) {
+    def possibleWins: Long = {
+      @tailrec
+      def loop(timeCharged: Long, acc: Long): Long = if (timeCharged < time) {
+        val travelTime = time - timeCharged
+        val distanceTravelled = travelTime * timeCharged
+        val newAcc = if (distanceTravelled > distance) acc + 1 else acc
+        loop(timeCharged + 1, newAcc)
+      } else acc
+      loop(1, 0)
+    }
+  }
+
+  def parseInput(l: List[String]): List[Race] = l match {
+    case ::(time, ::(distance, _)) =>
+      val r = "\\d+".r
+      val times = r.findAllMatchIn(time).toList.map(_.group(0).toLong)
+      val distances = r.findAllMatchIn(distance).toList.map(_.group(0).toLong)
+      times.zip(distances).map { case (t, d) => Race(t, d) }
+    case _ => List.empty
+  }
+
+  override def run(args: List[String]): IO[ExitCode] = Util.getInput
+    .use { bs =>
+      val races = parseInput(bs.getLines().toList)
+      IO.println(races.map(_.possibleWins).product)
+    }
+    .as(ExitCode.Success)
 }
